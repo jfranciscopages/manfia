45043a52df5b91ea8ea29dfb25027da94be3bd8e
zamuaoi <alexxudot@gmail.com>
2021-09-07 15:15:27 -0300

    H1 en App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> WEPP!!</div>;
+  return (
+    <div className="App">
+      WEPP!!
+      <h1> Holanda!!!</h1>
+    </div>
+  );
 }
 
 export default App;
