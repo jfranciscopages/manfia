7e5444ecd4d3c8ccb9466ee160bc248224bbabf4
Nata Peralta <82235767+NataPeralta@users.noreply.github.com>
2021-09-07 15:19:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> Se le rompio el code a Agus</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
+
+//hago un comentario malaso
