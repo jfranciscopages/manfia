5c3174c10bd979e224210f248e6dd0840f90d69d
zamuaoi <alexxudot@gmail.com>
2021-09-15 21:39:40 -0300

    agregar productos funcionando bien

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -20,7 +20,7 @@ import {
 import { AddIcon, MinusIcon, DeleteIcon } from "@chakra-ui/icons";
 
 function Cart() {
-  const { substractQuantity, addProductToCart, deleteProductCart, orderform } =
+  const { substractQuantity, addOneMoreProduct, deleteProductCart, orderform } =
     useCart();
 
   return (
@@ -33,105 +33,115 @@ function Cart() {
         justifyContent="center"
         alignItems="center"
       >
-      <Heading fontSize="2xl" textAlign="center" marginTop="20px" color="teal.400">
-        MI CARRITO
-      </Heading>
-       <Stack
+        <Heading
+          fontSize="2xl"
+          textAlign="center"
+          marginTop="20px"
+          color="teal.400"
+        >
+          MI CARRITO
+        </Heading>
+        <Stack
           flexDir="column"
           mb="2"
           justifyContent="center"
           alignItems="center"
         >
-      <Box display="flex" alignItems="center">
-      {orderform?
-        orderform.items.length > 0 ? (
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>Producto</Th>
-                <Th></Th>
-                <Th></Th>
-                <Th>Cantidad</Th>
-                <Th>Precio</Th>
-                <Th></Th>
-              </Tr>
-            </Thead>
-
-            <Tbody>
-              {orderform
-                ? orderform.items.map((product) => (
+          <Box display="flex" alignItems="center">
+            {orderform ? (
+              orderform.items.length > 0 ? (
+                <Table>
+                  <Thead>
                     <Tr>
-                      <Td>
-                        <Image src={product.image} boxSize="50px" />
-                      </Td>
-                      <Td fontSize="md">{product.title}</Td>
-                      <Td fontSize="xs">{product.description}</Td>
-                      <Td>
-                        <Stack spacing={4} direction="row" align="center">
-                          <Button
-                            colorScheme="teal"
-                            size="xs"
-                            onClick={() =>
-                              substractQuantity(product, product.quantity)
-                            }
-                          >
-                            <MinusIcon />
-                          </Button>
-                          <Box> {product.quantity} </Box>
-
-                          <Button
-                            colorScheme="teal"
-                            size="xs"
-                            onClick={() =>
-                              addProductToCart(product, product.quantity)
-                            }
-                          >
-                            <AddIcon />
-                          </Button>
-                        </Stack>
-                      </Td>
-                      <Td>${Number(product.price * product.quantity)}</Td>
-                      <Td>
-                        <Button
-                          colorScheme="teal"
-                          size="xs"
-                          onClick={() =>
-                            deleteProductCart(product, product.quantity)
-                          }
-                        >
-                          <DeleteIcon />
-                        </Button>
-                      </Td>
+                      <Th>Producto</Th>
+                      <Th></Th>
+                      <Th></Th>
+                      <Th>Cantidad</Th>
+                      <Th>Precio</Th>
+                      <Th></Th>
                     </Tr>
-                  ))
-                : ""}
-            </Tbody>
+                  </Thead>
 
-            <Tfoot>
-              <Tr>
-                <Th mr="4" fontSize="2xl">
-                  TOTAL
-                </Th>
+                  <Tbody>
+                    {orderform
+                      ? orderform.items.map((product) => (
+                          <Tr>
+                            <Td>
+                              <Image src={product.image} boxSize="50px" />
+                            </Td>
+                            <Td fontSize="md">{product.title}</Td>
+                            <Td fontSize="xs">{product.description}</Td>
+                            <Td>
+                              <Stack spacing={4} direction="row" align="center">
+                                <Button
+                                  colorScheme="teal"
+                                  size="xs"
+                                  onClick={() =>
+                                    substractQuantity(product, product.quantity)
+                                  }
+                                >
+                                  <MinusIcon />
+                                </Button>
+                                <Box> {product.quantity} </Box>
 
-                <Th mr="4" fontSize="2xl">$
-                  {orderform.items.reduce(function (eAnterior, eActual) {
-                    return eAnterior + Number(eActual.price * eActual.quantity);
-                  }, 0)}
-                </Th>
-              </Tr>
-            </Tfoot>
-          </Table>
-        ) : ""
-        : ""}
-      </Box>
-      <Box>
-        {orderform.items.length > 0 ? (
-          <Link to="/checkout">Checkout</Link>
-        ) : (
-          <Box>No hay productos en el carrito.</Box>
-        )}
-      </Box>
-      </Stack>
+                                <Button
+                                  colorScheme="teal"
+                                  size="xs"
+                                  onClick={() => addOneMoreProduct(product)}
+                                >
+                                  <AddIcon />
+                                </Button>
+                              </Stack>
+                            </Td>
+                            <Td>${Number(product.price * product.quantity)}</Td>
+                            <Td>
+                              <Button
+                                colorScheme="teal"
+                                size="xs"
+                                onClick={() =>
+                                  deleteProductCart(product, product.quantity)
+                                }
+                              >
+                                <DeleteIcon />
+                              </Button>
+                            </Td>
+                          </Tr>
+                        ))
+                      : ""}
+                  </Tbody>
+
+                  <Tfoot>
+                    <Tr>
+                      <Th mr="4" fontSize="2xl">
+                        TOTAL
+                      </Th>
+
+                      <Th mr="4" fontSize="2xl">
+                        $
+                        {orderform.items.reduce(function (eAnterior, eActual) {
+                          return (
+                            eAnterior + Number(eActual.price * eActual.quantity)
+                          );
+                        }, 0)}
+                      </Th>
+                    </Tr>
+                  </Tfoot>
+                </Table>
+              ) : (
+                ""
+              )
+            ) : (
+              ""
+            )}
+          </Box>
+          <Box>
+            {orderform.items.length > 0 ? (
+              <Link to="/checkout">Checkout</Link>
+            ) : (
+              <Box>No hay productos en el carrito.</Box>
+            )}
+          </Box>
+        </Stack>
       </Flex>
     </>
   );
